8b7768e2041ae6dd5ddb3f4a1667ae2a5d4a87bd
T0mSIlver <34662901+T0mSIlver@users.noreply.github.com>
2018-02-02 23:06:40 +0100

    ff
    ff

---
+++ dataframes.py
@@ -0,0 +1,107 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[94]:
+
+
+dfB = pd.read_excel('Black Cards.xlsx', sheet_name='Sheet1')
+
+
+# In[95]:
+
+
+dfB.head()
+
+
+# In[96]:
+
+
+dfW = pd.read_excel('White Cards.xlsx', sheet_name='Sheet1')
+
+
+# In[100]:
+
+
+#dfB.sample(n=None, frac=None, replace=False, weights=None, random_state=None, axis=None)
+
+
+# In[98]:
+
+
+#dfW.head()
+
+
+# In[36]:
+
+
+#sCardB = dfB.sample(n=None, frac=None, replace=False, weights=None, random_state=None, axis=None)
+
+
+# In[37]:
+
+
+#CardB = sCardB.drop('Card Color', 1)
+
+
+# In[38]:
+
+
+#CardB.head()
+
+
+# In[46]:
+
+
+#print(CardB.values,CardW.values)
+
+
+# In[44]:
+
+
+#sCardW = dfW.sample(n=None, frac=None, replace=False, weights=None, random_state=None, axis=None)
+
+
+# In[45]:
+
+
+#CardW = sCardW.drop('Card Color', 1)
+
+
+# In[55]:
+
+
+#for ___ in cardsB[1] :
+    sentence = card.replace('%%%', cardW[1])
+    print(sentence)
+
+
+# In[88]:
+
+
+#"playedW[playedW['Content']==0].values[0]
+
+
+# In[86]:
+
+
+#playedW = CardW
+
+
+# In[72]:
+
+
+#print(dfW.iloc[529])
+
+
+# In[87]:
+
+
+#print(playedW)
+
