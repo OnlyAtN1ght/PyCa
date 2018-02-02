c88f453c95ac7e048f63f2633bce11bef76cd659
T0mSIlver <34662901+T0mSIlver@users.noreply.github.com>
2018-02-03 00:29:15 +0100

    dfW

--- 10 random white cars in a list.py
+++ 10 random white cars in a list.py
@@ -1,3 +1,4 @@
+dfW = pd.read_excel('White Cards.xlsx', sheet_name='Sheet1')
 for i in range(0,10):
     scardW = dfW.sample(n=None, frac=None, replace=False, weights=None, random_state=None, axis=None)
     cardW = scardW.iloc[0]['Content']
